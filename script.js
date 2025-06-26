57ec7a257eaf969fb3f915776dfc41a34f2b811c
Adi96singh <iadi96singh@gmail.com>
2025-06-26 15:46:45 +0530

    Js file uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log(a);
+//var a = 5;
+//console.log(a);
+
+const arr=["apple", "peach", "banana","orange","grape"]; 
+const[red,blue,green,...rest] = arr;
+console.log(arr[4]);  
+console.log(rest); 
